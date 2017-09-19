82bb3ffd2360d8875d4b0a85fd1a1583b1c4bf01
anubhav91 <anubhav.abstain@gmail.com>
2017-09-19 15:12:55 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,15 +3,17 @@
 Plugin Name: Weather Widget
 Description: Weather Widget
 Author: Abstain Solutions
+Version:11
 */
 function load_custom_wp_admin_style() 				// adding scripts
 {
 	$url  = plugin_dir_url( __FILE__ ) ;
-    wp_register_style( 'weather_wp_admin_css', $url . 'css/weather-style.css', false, '1.0.0' );
+    wp_register_style( 'weather_wp_admin_css', $url . 'weather-style.css', false, '1.0.0' );
     wp_enqueue_style( 'weather_wp_admin_css' );
-	wp_enqueue_script( 'refresh.js',$url . 'js/refresh.js' ); //refresh div content		
+	wp_enqueue_script( 'refresh.js',$url . 'refresh.js' ); //refresh div content		
 }
 add_action( 'admin_enqueue_scripts', 'load_custom_wp_admin_style' );
+add_action( 'wp_enqueue_scripts', 'load_custom_wp_admin_style' );
 function weather_widget_main_function() 
 {
 	wp_add_dashboard_widget(
@@ -28,13 +30,13 @@ add_action( 'wp_dashboard_setup', 'weather_widget_main_function' );
 function kelvin_to_fahrenheit($given_value)		
 {
 	$fahrenheit=9/5*($given_value-273.15)+32;
-	return $fahrenheit ;
+	return  round($fahrenheit) ;
 }
 //Kelvin to celsius equation
 function kelvin_to_celsius($given_value)			
 {
 	$celsius=$given_value-273.15;
-	return $celsius ;
+	return  round($celsius) ;
 }
 function weather_child_function() 
 {
@@ -53,7 +55,7 @@ function weather_child_function()
 	$data .= '
 	<div id="parent_weather" class="parent_weather"><div><h2><center>'.$name.'</center></h2></div>';
 	$data .= '<div class="center"><img src="http://openweathermap.org/img/w/'.$image_name.'.png"></div>';
-	$data .= '<div class="center">Status : '.ucfirst($weather[0]->description).'</div>';
+	$data .= '<div class="center">Status : '.strtoupper($weather[0]->description).'</div>';
 	$data .= '<div class="center">Temprature (Fahrenheit) : '.kelvin_to_fahrenheit($main->temp).' F</div>';
 	$data .= '<div class="center">Temprature (Celsius) : '.kelvin_to_celsius($main->temp).' .C</div>';
 	$data .= '<ul class="cloud"><li>Clouds : '.$clouds->all.'%</li>';
@@ -64,4 +66,5 @@ function weather_child_function()
 	$data .= '<li class="right">Sunset : '.date('m/d/Y h:i:s A', $sys->sunset).'</li></ul></div>';
 	echo $data;
 }
+add_shortcode('weather_widget','weather_child_function');
 ?>
